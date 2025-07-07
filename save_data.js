d16c5822b4a082d9d3510c0b1741a860020d7cea
canyonjang <114268359+canyonjang@users.noreply.github.com>
2025-07-07 17:32:38 +0900

    Update save_data.js

--- save_data.js
+++ save_data.js
@@ -2,7 +2,6 @@
 function saveDataToSheet(data) {
   const formURL = "https://docs.google.com/forms/d/e/1FAIpQLSe_HThhsN0Ce8ysQvTE1fdrjwBkxjmzRw1J7xZl_Ee_LVkF2w/formResponse";
 
-  // ✅ 각 항목의 Entry ID (Google Form 실행 로그에서 복사한 값)
   const entryIDs = {
     subject_id: "932143536",
     condition: "1841598794",
@@ -13,7 +12,6 @@ function saveDataToSheet(data) {
     correct: "380677287"
   };
 
-  // ✅ trial 하나씩 전송 (속도 조절)
   data.forEach((trial, i) => {
     console.log("전송할 trial:", trial);
 
@@ -26,14 +24,12 @@ function saveDataToSheet(data) {
     formData.append(`entry.${entryIDs.rt}`, trial.rt || "");
     formData.append(`entry.${entryIDs.correct}`, trial.correct || "");
 
-    // 약간의 시간차 두고 전송
     setTimeout(() => {
       fetch(formURL, {
         method: "POST",
         mode: "no-cors",
         body: formData
       });
-    }, i * 300); // 0.3초 간격
+    }, i * 300);
   });
 }
-
