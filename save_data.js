6460628dc93cf4d5b3a3cd5491338db41d5497c8
canyonjang <114268359+canyonjang@users.noreply.github.com>
2025-07-07 16:14:36 +0900

    Update save_data.js

--- save_data.js
+++ save_data.js
@@ -1,19 +1,10 @@
 // 전체 데이터 저장 함수
 function saveDataToSheet(data) {
-  fetch("https://script.google.com/macros/s/AKfycbzyfakGcKLwqeLMO9GvtZ2zp0Y1e1R2PfxM1DpUX4YFyT2X-D97WGICRrQ2wA8rGrIpkg/exec", {
+  fetch("https://script.google.com/macros/s/AKfycbw-KrRW94QEVjcGkegF7JsYzXFVXKR8jsLUTmw8NfC26iXLbqs8aH0tUlUubyiXiPWT/exec", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify(data)
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("서버 응답 오류");
-    }
-    console.log("✅ Google Sheet로 데이터 전송 성공");
-  })
-  .catch(error => {
-    console.error("❌ 데이터 전송 실패:", error);
   });
 }
