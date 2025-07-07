66c33be76d7cee6ae4bbbc19b49e924c12378157
canyonjang <114268359+canyonjang@users.noreply.github.com>
2025-07-07 15:10:00 +0900

    Update save_data.js

--- save_data.js
+++ save_data.js
@@ -1,12 +1,10 @@
-// 데이터 저장: Google Sheet 연동용 URL 필요
-jsPsych.init({
-  on_data_update: function(data) {
-    fetch("https://script.google.com/macros/s/AKfycbwldjJxbsINse6inWpdAG3YP30UubMghkDS6pdi7mjAdhwriRefyI-m0jiF6wymL--RZA/exec", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
-  }
-});
+// 전체 데이터 저장 함수
+function saveDataToSheet(data) {
+  fetch("https://script.google.com/macros/s/AKfycbxCeYdGMlbgWYYMVBKSNm-xXblsnXotGYPMZAjLzK_2EzMXLrJxsMMjbFNncFl_QmyhxQ/exec", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+}
