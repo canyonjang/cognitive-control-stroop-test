18fe3ca2338d0c7575e7c1a6c78b5435cb5633f7
canyonjang <114268359+canyonjang@users.noreply.github.com>
2025-07-07 14:15:31 +0900

    Update stroop_numeric.js

--- stroop_numeric.js
+++ stroop_numeric.js
@@ -7,20 +7,20 @@ var stroop_stimuli = [
   {stimulus: "5 5 5 5 5", correct_response: "5"}
 ];
 
-var stroop_trials = stroop_stimuli.map(function(trial, index) {
+var stroop_trials = stroop_stimuli.map(function(trial, idx) {
   return {
     type: 'html-keyboard-response',
     stimulus: `<p style="font-size: 40px;">${trial.stimulus}</p>`,
-    choices: ['1','2','3','4','5'],
+    choices: ['1', '2', '3', '4', '5'],
     data: {
       task: 'stroop',
-      correct: trial.correct_response,
-      trial_index: index,
-      stimulus: trial.stimulus,
+      subject_id: subject_id,      // ✅ 추가
       condition: condition,
-      subject_id: subject_id
+      stimulus: trial.stimulus,
+      correct: trial.correct_response,
+      trial_index: idx
     },
-    on_finish: function(data){
+    on_finish: function(data) {
       data.correct = data.response == data.correct;
     }
   };
