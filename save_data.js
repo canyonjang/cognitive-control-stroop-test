9a831b4f6707324c2b7351e16da75bd863a0143a
canyonjang <114268359+canyonjang@users.noreply.github.com>
2025-07-07 15:22:23 +0900

    Update save_data.js

--- save_data.js
+++ save_data.js
@@ -1,6 +1,6 @@
 // 전체 데이터 저장 함수
 function saveDataToSheet(data) {
-  fetch("https://script.google.com/macros/s/AKfycbxCeYdGMlbgWYYMVBKSNm-xXblsnXotGYPMZAjLzK_2EzMXLrJxsMMjbFNncFl_QmyhxQ/exec", {
+  fetch("https://script.google.com/macros/s/AKfycbxfym_6mzpBpmGVEcyjdqw7ejEOq6zgH7swJflKufxIgcx37lW-_BSI9QJJXtIu8HBU5Q/exec", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
