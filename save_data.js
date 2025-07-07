a02bc14ebb17212cb07a1453f349a21eb9b84bd9
canyonjang <114268359+canyonjang@users.noreply.github.com>
2025-07-07 16:41:57 +0900

    Update save_data.js

--- save_data.js
+++ save_data.js
@@ -1,6 +1,6 @@
 // ✅ Google Form으로 데이터 전송 함수
 function saveDataToSheet(data) {
-  const formURL = "https://docs.google.com/forms/d/e/1FAIpQLSe_HThhsN0Ce8ysQvTE1fdRjwBkxjmzRwJJ7xZ1_Ee_LVkF2w/formResponse";
+  const formURL = "https://docs.google.com/forms/d/e/1FAIpQLSe_HThhsN0Ce8ysQvTElfdrjwBkxjmzRw1J7xZl_Ee_LVkF2w/formResponse";
 
   // ✅ 각 항목의 Entry ID (Google Form 실행 로그에서 복사한 값)
   const entryIDs = {
